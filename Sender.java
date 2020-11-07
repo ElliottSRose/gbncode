0b99d22b656051054a8c9a584e16a12bd865b796
Laura <30821287+lauralaura14@users.noreply.github.com>
2020-11-06 20:30:46 -0500

    added some timeout code

--- Sender.java
+++ Sender.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
@@ -34,20 +35,12 @@ public class Sender {
     private static ByteBuffer buf;
 
     public static void main(String args[]) throws IOException {
-        DatagramSocket ds = new DatagramSocket();
-        InetAddress ip = InetAddress.getLocalHost();
-
-        DatagramPacket pkt;
-        File file = new File("test.txt");
-        FileInputStream fis = new FileInputStream(file);
-        BufferedInputStream bis = new BufferedInputStream(fis);
-        int bytesRead = 0;
 
         byte[] totalBytes = Files.readAllBytes(Paths.get("test.txt")); //convert entire file to bytes
 
         //totalPackets = totalBytes.length / max; //total # of packets in file
         //ArrayList<Packet> packetList = new ArrayList<>(); //new list of all packets
-        //DatagramPacket ack = new DatagramPacket(); //create new Datagram packet for ACK coming in -- need to fill in parameters
+        DatagramPacket ack = new DatagramPacket(); //create new Datagram packet for ACK coming in -- need to fill in parameters
         byte[] ackBytes = new byte[200]; //arbitrary number for ACK bytes
 
         //user inputs number 0-99
@@ -65,69 +58,88 @@ public class Sender {
             }
         } while (userNum >= 0 && userNum <= 99 || testNum == -1);
 
-        while (true) {
-            start = System.nanoTime(); //start the timer
-            int pseudoNum = new Random(System.currentTimeMillis()).nextInt(); //pseudonumber generated using random seed set to current system time
+        try {
+            DatagramSocket ds = new DatagramSocket();
+            InetAddress ip = InetAddress.getLocalHost();
 
-            currentSeqNum = 0;
-            nextSeqNum = 0;
+            DatagramPacket pkt;
+            File file = new File("test.txt");
+            FileInputStream fis = new FileInputStream(file);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+            int bytesRead = 0;
 
-            while (currentSeqNum < windowSize - 1) {  //should not exceed window size
-                data = new byte[max];
-                startIndex = max * currentSeqNum;
-                endIndex = startIndex + max;
-                data = Arrays.copyOfRange(totalBytes, startIndex, endIndex); //get bytes for the current packet from totalBytes
+            while (true) {
+                start = System.nanoTime(); //start the timer
+                int pseudoNum = new Random(System.currentTimeMillis()).nextInt(); //pseudonumber generated using random seed set to current system time
 
-                buf = ByteBuffer.wrap(data);
+                currentSeqNum = 0;
+                nextSeqNum = 0;
 
-                buf.clear();
-                bytesRead = bis.read(data);
+                while (currentSeqNum < windowSize - 1) {  //should not exceed window size
+                    data = new byte[max];
+                    startIndex = max * currentSeqNum;
+                    endIndex = startIndex + max;
+                    data = Arrays.copyOfRange(totalBytes, startIndex, endIndex); //get bytes for the current packet from totalBytes
 
-                if (bytesRead == -1) {
-                    break;
+                    buf = ByteBuffer.wrap(data);
+
+                    buf.clear();
+                    bytesRead = bis.read(data);
+
+                    if (bytesRead == -1) {
+                        break;
+                    }
+
+                    //if random number generated is less than user input, then simulate packet loss
+                    if (pseudoNum < userNum) {
+                        ++packetLoss; //keep count of total packet losses
+
+                    } else {
+                        buf.rewind();
+                        pkt = new DatagramPacket(data, max, ip, 8888);
+                        ds.send(pkt);
+
+                        ++currentSeqNum;
+                        nextSeqNum = currentSeqNum + 1;
+                        ++totalPacketsSent;
+                    }
+
                 }
 
-                //if random number generated is less than user input, then simulate packet loss
-                if (pseudoNum < userNum) {
-                    ++packetLoss; //keep count of total packet losses
+                //if ds.receive(ack)
+                //need code for: if ACK is received within a specific timeframe, it is successful
+                //else if it takes too long/ACK never comes through,
+                //resend from the packet requiring ACK, up to the previous sequence number (nextSeqNum-1)
+                ds.setSoTimeout(20000); //arbitrary milliseconds
 
-                } else {
-                    buf.rewind();
-                    pkt = new DatagramPacket(data, max, ip, 8888);
-                    ds.send(pkt);
+                try {
+                    ds.receive(ack);
+                } catch (SocketTimeoutException e) {
+                    for (int i = (nextSeqNum - 1) - windowSize; i < nextSeqNum - 1; ++i) { //i'm attempting to make i = packet trying to get ACK'ed
+                        startIndex = max * i;
+                        endIndex = startIndex + max;
+                        data = Arrays.copyOfRange(totalBytes, startIndex, endIndex);
 
-                    ++currentSeqNum;
-                    nextSeqNum = currentSeqNum + 1;
-                    ++totalPacketsSent;
+                        buf = ByteBuffer.wrap(data);
+
+                        buf.clear();
+                        bytesRead = bis.read(data);
+                        buf.rewind();
+                        pkt = new DatagramPacket(data, max, ip, 8888);
+                        ds.send(pkt);
+
+                        ++currentSeqNum;
+                        nextSeqNum = currentSeqNum + 1;
+                    }
                 }
-
             }
-
-            //if ds.receive(ack)
-            //need code for: if ACK is received within a specific timeframe, it is successful
-            //else if it takes too long/ACK never comes through,
-            //resend from the packet requiring ACK, up to the previous sequence number (nextSeqNum-1)
-            for (int i = (nextSeqNum - 1) - windowSize; i < nextSeqNum - 1; ++i) { //i'm attempting to make i = packet trying to get ACK'ed
-                startIndex = max * i;
-                endIndex = startIndex + max;
-                data = Arrays.copyOfRange(totalBytes, startIndex, endIndex);
-
-                buf = ByteBuffer.wrap(data);
-
-                buf.clear();
-                bytesRead = bis.read(data);
-                buf.rewind();
-                pkt = new DatagramPacket(data, max, ip, 8888);
-                ds.send(pkt);
-
-                ++currentSeqNum;
-                nextSeqNum = currentSeqNum + 1;
-            }
+            end = System.nanoTime(); //end the timer
+            bis.close();
+            fis.close();
+            ds.close();
+        } catch (SocketException e) {
+            System.out.println("Socket is now closed.");
         }
-
-        bis.close();
-        fis.close();
-        end = System.nanoTime(); //end the timer
 
         System.out.println("Elapsed time: " + (end - start));
         System.out.println("Packets sent: " + currentSeqNum);
