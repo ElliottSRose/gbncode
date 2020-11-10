9abd7a279f1a27afaab2ec06febcc26a0f3ae991
Elliott Rose <45501305+ElliottSRose@users.noreply.github.com>
2020-11-10 12:44:50 -0800

    Update Sender.java
    Hi Laura, I made some changes and things are running better. I've tested it with the udpServer file and the first packets are received. Now we just need to make the Server send back the acknowledgements and rejections.

--- Sender.java
+++ Sender.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,7 +10,6 @@ import java.net.SocketTimeoutException;
 import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -20,7 +18,6 @@ public class Sender {
 
     private static int windowSize = 4;
     private static int currentSeqNum = 0;
-    private static int nextSeqNum = 0;
     //private static int totalPackets = 0;
     private static int totalPacketsSent = 0;
     private static long start = 0;
@@ -35,115 +32,89 @@ public class Sender {
     private static ByteBuffer buf;
     private static int lostSeqNum;
 
-    public static void main(String args[]) throws IOException {
-
-        byte[] totalBytes = Files.readAllBytes(Paths.get("test.txt")); //convert entire file to bytes
-
-        //totalPackets = totalBytes.length / max; //total # of packets in file
-        //ArrayList<Packet> packetList = new ArrayList<>(); //new list of all packets
-        DatagramPacket ack = new DatagramPacket(); //create new Datagram packet for ACK coming in -- need to fill in parameters
-        byte[] ackBytes = new byte[200]; //arbitrary number for ACK bytes
-
+    static int packetLossSim() {
         //user inputs number 0-99
         Scanner reader = new Scanner(System.in);
-
         do {
             System.out.println("Please enter a number from 0-99:");
             userNum = Integer.parseInt(reader.nextLine());
-
             if (userNum < 0 || userNum > 99) {
                 System.out.println("Invalid. Please enter a number from 0-99: \n");
                 testNum = -1;   //wrong input = loop again
             } else if (userNum >= 0 && userNum <= 99) {
-                break;
+                return userNum;
             }
         } while (userNum >= 0 && userNum <= 99 || testNum == -1);
+        return userNum;
+    }
 
+    public static void main(String args[]) throws IOException {
+        byte[] totalBytes = Files.readAllBytes(Paths.get("test.txt")); //convert entire file to bytes
+        //totalPackets = totalBytes.length / max; //total # of packets in file
+        //ArrayList<Packet> packetList = new ArrayList<>(); //new list of all packets
+        byte[] ackBytes = new byte[200]; //arbitrary number for ACK bytes
+        DatagramPacket ack = new DatagramPacket(ackBytes, ackBytes.length); //create new Datagram packet for ACK coming in -- need to fill in parameters
         try {
             DatagramSocket ds = new DatagramSocket();
+            ds.setSoTimeout(2000); //arbitrary milliseconds
             InetAddress ip = InetAddress.getLocalHost();
-
             DatagramPacket pkt;
             File file = new File("test.txt");
             FileInputStream fis = new FileInputStream(file);
             BufferedInputStream bis = new BufferedInputStream(fis);
             int bytesRead = 0;
-
-            start = System.nanoTime(); //start the timer
-
+//            start = System.nanoTime(); //start the timer
             while (true) {
                 int pseudoNum = new Random(System.currentTimeMillis()).nextInt(); //pseudonumber generated using random seed set to current system time
-                
                 int eachRoundCompare = currentSeqNum + windowSize;
-                
+                //user inputs number 0-99
+                userNum = packetLossSim();
                 while (currentSeqNum < eachRoundCompare) {  //should not exceed window size
                     data = new byte[max];
                     startIndex = max * currentSeqNum;
                     endIndex = startIndex + max;
                     data = Arrays.copyOfRange(totalBytes, startIndex, endIndex); //get bytes for the current packet from totalBytes
-
                     buf = ByteBuffer.wrap(data);
-
                     buf.clear();
                     bytesRead = bis.read(data);
-
                     if (bytesRead == -1) {
+                        bis.close();
+                        fis.close();
+                        ds.close();
+                        System.out.println("Packets sent: " + totalPacketsSent);
+                        System.out.println("Lost packets: " + packetLoss);
+    //                    end = System.nanoTime(); //end the timer -- unreachable statement error
+    //                    System.out.println("Elapsed time: " + (end - start));
                         break;
-                    }
-
+                }
                     //if random number generated is less than user input, then simulate packet loss
-                    if (pseudoNum < userNum) {
-                        ++packetLoss; //keep count of total packet losses
-                        lostSeqNum = currentSeqNum;
-
-                    } else {
+                    else {
                         buf.rewind();
                         pkt = new DatagramPacket(data, max, ip, 8888);
-                        ds.send(pkt);
+                        if (pseudoNum < userNum) {
+                            ++packetLoss; //keep count of total packet losses
+                            lostSeqNum = currentSeqNum;
+                            //if ACK is received within a specific timeframe, it is successful
+                            //else if it takes too long/ACK never comes through,
+                            //resend from the packet requiring ACK, up to the previous sequence number
+                        } else {
+                            ds.send(pkt);
+                            ++currentSeqNum;
+                            ++totalPacketsSent;
+                            }
+                        if (currentSeqNum==eachRoundCompare) {
+                            try {
+                                ds.receive(ack);
+                            } catch (SocketTimeoutException e) {
+                                System.out.println("Timeout error, resend packets from: " + lostSeqNum);
+                            }
+                        }
                     }
-                    ++currentSeqNum;
-                    nextSeqNum = currentSeqNum + 1;
-                    ++totalPacketsSent; 
-                    //maybe create a list of all the packets sent
-                }
-
-                //if ACK is received within a specific timeframe, it is successful
-                //else if it takes too long/ACK never comes through,
-                //resend from the packet requiring ACK, up to the previous sequence number
-                ds.setSoTimeout(20000); //arbitrary milliseconds
-
-                try {
-                    ds.receive(ack);
-                } catch (SocketTimeoutException e) {
-                    for (int i = currentSeqNum; i < currentSeqNum; ++i) { //incorrect. i'm attempting to make i = packet trying to get ACK'ed -- need help
-                        startIndex = max * i;
-                        endIndex = startIndex + max;
-                        data = Arrays.copyOfRange(totalBytes, startIndex, endIndex);
-
-                        buf = ByteBuffer.wrap(data);
-
-                        buf.clear();
-                        bytesRead = bis.read(data);
-                        buf.rewind();
-                        pkt = new DatagramPacket(data, max, ip, 8888);
-                        ds.send(pkt);
-
-                        ++currentSeqNum;
-                        nextSeqNum = currentSeqNum + 1;
                     }
                 }
-            }
-            end = System.nanoTime(); //end the timer -- unreachable statement error
-            System.out.println("Elapsed time: " + (end - start));
-            bis.close();
-            fis.close();
-            ds.close();
-        } catch (SocketException e) {
-            System.out.println("Socket is closed.");
+    }
+        finally {
+            System.out.println("Goodbye!");
         }
 
-        System.out.println("Packets sent: " + totalPacketsSent);
-        System.out.println("Lost packets: " + packetLoss);
-    }
-
-}
+}};
